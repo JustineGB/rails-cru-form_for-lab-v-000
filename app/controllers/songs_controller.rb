ff57e26669cc39b0d7220849da9fe12c858bcced
Justine B <justinegayton@gmail.com>
2018-12-21 19:50:22 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,29 @@
+class SongsController < ApplicationController
+  
+  def index
+    @songs = Song.all
+  end
+    
+  def new 
+    @song = Song.new
+  end 
+  
+  def show 
+    @song = Song.find(params[:id])
+  end
+  
+  def create
+     @song = Song.create(params.require(:song))
+        redirect_to song_path(@song)
+  end 
+  
+  def edit 
+    @song = Song.find(params[:id])
+  end 
+  
+  def update
+    @song = Song.find(params[:id])
+    @songs.update(params.require(:song))
+    redirect_to song_path(@song)
+  end 
+end 
