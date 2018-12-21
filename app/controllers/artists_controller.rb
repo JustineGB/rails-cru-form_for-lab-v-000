c6d33af26ce5166f19158b9be4b1940b174ea90a
Justine B <justinegayton@gmail.com>
2018-12-21 20:02:23 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,8 +13,8 @@ class ArtistsController < ApplicationController
   end 
   
   def create
-     @artist = Artist.create(name: params[:name], bio: params[:bio])
-        redirect_to artist_path(@artist)
+    @artist = Artist.create(name: params[:name], bio: params[:bio])
+    redirect_to artist_path(@artist)
   end 
   
   def edit 
